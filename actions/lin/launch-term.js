523cdaa6d99fc9345da48524b15a849779875308
R-Nbt <rubenabet@gmail.com>
2020-05-09 19:36:41 +0200

    Refactor action files

---
+++ actions/lin/launch-term.js
@@ -0,0 +1,12 @@
+export const script = [
+    'GUI',
+    'DELAY 250',
+    'STRING [VOTRE PROGRAMME TERMINAL ICI]',
+    'DELAY 250',
+    'ENTER',
+    'DELAY 450',
+    'STRING [VOTRE CHAINE DE CARACTERES ICI]',
+    'DELAY 250',
+    'ENTER',
+    'DELAY 5000'
+]
---
+++ actions/win/launch-pwsh.js
@@ -0,0 +1,13 @@
+export const script = [
+    'DELAY 3000',
+    'GUI R',
+    'DELAY 250',
+    'STRING powershell',
+    'DELAY 250',
+    'ENTER',
+    'DELAY 450',
+    "STRING [VOTRE CHAINE DE CARACTERES ICI]",
+    'DELAY 250',
+    'ENTER',
+    'DELAY 5000',
+]
